88575c81ddf34a0e5e33a6d9a53e18dd6befaed9
Wade Mauger <wade_mauger@intuit.com>
2016-08-10 22:31:41 -0700

    Take Input elements into consideration. UGH.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var el = document.createElement('script');
-el.text = 'document.addEventListener("keydown", (event)=>{if(event.keyCode===8){window.history.back();}}, false);';
+el.text = '\
+document.addEventListener("keydown", (event)=>{ \
+  if(event.keyCode===8){ \
+    if(!(document.activeElement.tagName === "INPUT")){\
+      window.history.back(); \
+    }\
+  }}, false);  \
+';
 document.documentElement.appendChild (el);
+

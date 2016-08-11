4bdc1e101868a4a63894b320fb850840cd1c4505
Wade Mauger <wade_mauger@intuit.com>
2016-08-10 22:49:32 -0700

    Minor improvements

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var el = document.createElement('script');
 el.text = '\
 document.addEventListener("keydown", (event)=>{ \
-  if(event.keyCode===8){ \
-    if(!(document.activeElement.tagName === "INPUT")){\
+  if(event.keyCode === 8){ \
+    /*console.log("Backspace pressed...");*/\
+    var el = document.activeElement; \
+    if((el.tagName !== "INPUT") && (el.contentEditable !== "true")){\
       window.history.back(); \
     }\
+    /*console.log("...But we chose not to go back.");*/\
   }}, false);  \
 ';
 document.documentElement.appendChild (el);
